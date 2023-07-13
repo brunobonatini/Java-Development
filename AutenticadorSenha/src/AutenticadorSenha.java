868ad317568e72c2860380af93181560b8c76a36
Bruno Spagnol Bonatini <brunospagnol@gmail.com>
2023-07-12 23:51:03 -0300

    Add program

--- AutenticadorSenha/src/AutenticadorSenha.java
+++ AutenticadorSenha/src/AutenticadorSenha.java
@@ -2,10 +2,28 @@
 incorreta informada, escrever a mensagem "Senha Invalida". Quando a senha for informada corretamente deve ser 
 impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002. */
 
+import java.util.Scanner;
+
 public class AutenticadorSenha {
 
 	public static void main(String[] args) {
 		
+		Scanner input = new Scanner(System.in);
+				
+		System.out.print("Digite a senha correta: ");
+		int senha = input.nextInt();
+		
+		int senhaCorreta = 2002;
+		
+		while (senha != 2002) {
+			System.out.println("Senha incorreta!");
+			System.out.print("Digite a senha correta: ");
+			senha = input.nextInt();
+		}
+		
+		System.out.print("Acesso permitido!");
+		
+		input.close();
 
 	}
 
